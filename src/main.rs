86d0af36e9fe9d1116eedc1a7d6ca145a0417909
Kacper Łęczyński <kacper.leczynski@wpengine.com>
2026-02-12 09:40:43 +0100

    refactor: main handle running application added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Error;
+
+use zero2prod::run;
+use zero2prod::start_listen;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let listener = start_listen();
+    run(listener)?.await
 }
