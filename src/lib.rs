f9e2d03572e32aa1150ec8995ffee6fae0f576d4
Kacper Łęczyński <kacper.leczynski@wpengine.com>
2026-02-13 16:35:54 +0100

    feat: subscribe endpoint added with expected fields to be deserialise

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 use std::{io::Error, net::TcpListener};
 
-use actix_web::{App, HttpResponse, HttpServer, Responder, dev::Server, web};
+use actix_web::{App, HttpResponse, HttpServer, dev::Server, web};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct FormaData {
+    email: String,
+    name: String,
+}
 
 pub fn start_listen() -> TcpListener {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random address");
@@ -8,13 +15,21 @@ pub fn start_listen() -> TcpListener {
     listener
 }
 
-async fn health_check() -> impl Responder {
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: web::Form<FormaData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, Error> {
-    let server = HttpServer::new(|| App::new().route("health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
